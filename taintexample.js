168822f00abdefc8524bad36310a5c50e826cc2f
Zubairahmad09 <78538043+Zubairahmad09@users.noreply.github.com>
2021-05-10 15:42:18 +0200

    Update taintexample.js

--- taintexample.js
+++ taintexample.js
@@ -6,19 +6,19 @@ function initClickCount() {
     }
 }
 
-// function setClickCount(x1) {
-//     window.localStorage.setItem("clickCount", x1);
-// }
+ function setClickCount(x1) {
+     window.localStorage.setItem("clickCount", x1);
+ }
 
-// function updateClickCount() {
-//     var clickCount = +window.localStorage.getItem("clickCount") || 0;
-//     var newClickCount = 0;
-//     if (clickCount >= 0) {
-//         newClickCount = clickCount + 1;
-//     }
-//     // window.localStorage.setItem("clickCount", newClickCount);
-//     setClickCount(newClickCount);
-// }
+ function updateClickCount() {
+     var clickCount = +window.localStorage.getItem("clickCount") || 0;
+     var newClickCount = 0;
+     if (clickCount >= 0) {
+         newClickCount = clickCount + 1;
+     }
+     // window.localStorage.setItem("clickCount", newClickCount);
+     setClickCount(newClickCount);
+ }
 
 initClickCount();
 
