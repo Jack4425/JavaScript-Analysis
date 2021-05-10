957816e973d7425ad510aa2cef60b0da4933d939
Zubairahmad09 <78538043+Zubairahmad09@users.noreply.github.com>
2021-05-10 15:39:41 +0200

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function initClickCount() {
+    if (!window.localStorage.getItem("clickCount")) {
+        window.localStorage.setItem("clickCount", 0);
+    }
+}
+
+// function setClickCount(x1) {
+//     window.localStorage.setItem("clickCount", x1);
+// }
+
+// function updateClickCount() {
+//     var clickCount = +window.localStorage.getItem("clickCount") || 0;
+//     var newClickCount = 0;
+//     if (clickCount >= 0) {
+//         newClickCount = clickCount + 1;
+//     }
+//     // window.localStorage.setItem("clickCount", newClickCount);
+//     setClickCount(newClickCount);
+// }
+
+initClickCount();
+
+
